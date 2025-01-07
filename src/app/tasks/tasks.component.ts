ead51fb2f14963af93a263bb7419447ac49b9038
ishuj15 <Ishu.Jain@watchguard.com>
2025-01-07 18:03:08 +0530

    2-way binding implemented

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,18 @@
 import { Component,Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
-
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskData } from './task/task.model';
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({required:true}) userId!:string;
   @Input({required:true})  name  :string |undefined;
+  isAddingTask = false;
+  
   tasks =[
     {
       id: 't1',
@@ -40,4 +43,23 @@ export class TasksComponent {
     return this.tasks.filter((task) => task.userId === this.userId);
   }
 
+  onCompleteTask(id:string){
+    this.tasks=this.tasks.filter((task) => task.id !==id);
+  }
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    })
+    this.isAddingTask = false;
+  }
 }
