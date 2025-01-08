026a5ba572ced6031f6b460451c56d1ab43aba1e
ishuj15 <Ishu.Jain@watchguard.com>
2025-01-08 08:17:18 +0530

    implemented date pipe

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,8 +1,9 @@
 import { Component , EventEmitter, Input, Output } from '@angular/core';
 import {Task} from './task.model';
+import {DatePipe} from '@angular/common';
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
